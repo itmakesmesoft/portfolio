11f168130a25eb696b80fc85d3ca3766f36d4f95
Daniel LEE <dmsgur7112@gmail.com>
2023-05-26 23:56:51 +0900

    fix : 폴더 구조 변경

--- src/pages/MainPage/Components/IntroPage.tsx
+++
@@ -1,9 +0,0 @@
-export const IntroPage = () => {
-  return (
-    <div className="w-full h-screen flex flex-col justify-center">
-      <p className="text-center text-[5rem] sm:text-[10rem] lg:text-[15rem] font-['Bangers']">
-        PORTFOLIO
-      </p>
-    </div>
-  );
-};
--- src/util/share.ts
+++
@@ -1,8 +0,0 @@
-export type InfoType = {
-  title: string;
-  description: string;
-  period: string;
-  memberInfo: string;
-  mainFunction: string[];
-  tech: string[];
-};
