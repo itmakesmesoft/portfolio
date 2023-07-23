8e62481de4e6eeae2fc8e4a492bd6a7f7813681a
Daniel LEE <dmsgur7112@gmail.com>
2023-07-24 00:51:35 +0900

    feat : 링크 및 호버링 효과 적용

--- portfolio/src/components/Information.tsx
+++ portfolio/src/components/Information.tsx
@@ -1,21 +1,33 @@
 import { MdFace, MdOutlineSmartphone, MdEmail } from "react-icons/md";
 import styled from "styled-components";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Information = () => {
   return (
     <div className="mt-[1.5rem] text-sm sm:text-base">
-      <Phrase>
-        <MdFace className="icon" />
-        이은혁
-      </Phrase>
-      <Phrase>
-        <MdOutlineSmartphone className="icon" />
-        010-6396-7078
-      </Phrase>
-      <Phrase>
-        <MdEmail className="icon" />
-        dmsgur7112@naver.com
-      </Phrase>
+      <Link
+        to="/#about"
+        scroll={(el) =>
+          el.scrollIntoView({ behavior: "smooth", block: "start" })
+        }
+      >
+        <Phrase>
+          <MdFace className="icon" />
+          이은혁
+        </Phrase>
+      </Link>
+      <a href="tel:010-6396-7078">
+        <Phrase>
+          <MdOutlineSmartphone className="icon" />
+          010-6396-7078
+        </Phrase>
+      </a>
+      <a href="mailto:dmsgur7112@naver.com">
+        <Phrase>
+          <MdEmail className="icon" />
+          dmsgur7112@naver.com
+        </Phrase>
+      </a>
       <p className="mt-8">Copyright 2023. Eunhyeok Lee. All rights reserved.</p>
     </div>
   );
@@ -29,6 +41,9 @@ const Phrase = styled.p`
   color: #616161;
   & .icon {
     margin-right: 0.5rem;
+  }
+  &:hover {
+    color: #45c0bb;
   }
 `;
 
