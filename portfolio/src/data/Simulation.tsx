115e818e55fe54d63a085d6bc1ea8d28615c1127
Daniel LEE <dmsgur7112@gmail.com>
2023-08-01 00:44:21 +0900

    fix : 그림자 수정

--- portfolio/src/data/Simulation.tsx
+++ portfolio/src/data/Simulation.tsx
@@ -4,9 +4,9 @@ const Simulation = (props: { data: string[][] }) => {
   const imageSrc: string[][] = props.data;
 
   return (
-    <div className="w-full max-w-[700px] py-[5rem] lg:py-[10rem] px-10 grid gap-4 grid-cols-2 content-center">
+    <div className="w-full max-w-[700px] py-[5rem] lg:py-[10rem] px-12 sm:px-20 grid gap-4 md:gap-6 grid-cols-2 content-center">
       {imageSrc.map((src: string[], i: number) => (
-        <WrapCard key={i} className="rounded-lg">
+        <WrapCard key={i} className="rounded-lg sm:rounded-xl">
           <Img src={src[1]} alt={`simulationImage_${i + 1}`} />
           <Img
             src={src[0]}
@@ -25,19 +25,20 @@ const WrapCard = styled.div`
   height: auto;
   aspect-ratio: 16/10;
   overflow: hidden;
-  box-shadow: #00000069 0 5px 20px -10px;
+  box-shadow: #00000069 0 5px 15px -5px;
   transition-duration: 0.5s;
+  cursor: pointer;
 
   &:hover .hovered {
     display: block;
   }
   &:nth-child(even):hover {
-    transform: scale(1.2) translateY(-15%) translateX(12%);
-    box-shadow: #00000069 -10px 10px 30px -10px;
+    transform: scale(1.12) translateY(-15%) translateX(12%);
+    box-shadow: #00000050 -10px 10px 30px -10px;
   }
   &:nth-child(odd):hover {
-    transform: scale(1.2) translateY(-15%) translateX(-12%);
-    box-shadow: #00000069 10px 10px 30px -10px;
+    transform: scale(1.12) translateY(-15%) translateX(-12%);
+    box-shadow: #00000050 10px 10px 30px -10px;
   }
 `;
 
