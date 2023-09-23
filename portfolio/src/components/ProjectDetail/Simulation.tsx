3a25f83dc3ef3c6c9b557d422e9ccaf993c9682e
Daniel LEE <dmsgur7112@gmail.com>
2023-09-23 18:56:50 +0900

    feat : 시뮬레이션 컴포넌트 hovering 이벤트 기본값 설정

--- portfolio/src/components/ProjectDetail/Simulation.tsx
+++ portfolio/src/components/ProjectDetail/Simulation.tsx
@@ -1,13 +1,33 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const Simulation = (props: { data: string[][] }) => {
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    if (index === -1) {
+      const debounce = setTimeout(() => {
+        setIndex(0);
+      }, 1000);
+      return () => {
+        console.log("clear");
+        clearTimeout(debounce);
+      };
+    }
+  }, [index]);
   const imageSrc: string[][] = props.data;
 
   return (
     <div className="w-full max-w-[700px] py-[5rem] lg:py-[10rem] px-12 sm:px-20 grid gap-4 md:gap-6 grid-cols-2 content-center">
       {imageSrc.map((src: string[], i: number) => {
         return (
-          <WrapCard key={i} className="rounded-lg sm:rounded-xl">
+          <WrapCard
+            className={`rounded-lg sm:rounded-xl ${
+              index === i ? "active" : ""
+            }`}
+            key={i}
+            onMouseOver={() => setIndex(i)}
+            onMouseLeave={() => setIndex(-1)}
+          >
             <Img src={src[1]} alt={`simulationImage_${i + 1}`} />
             <Img
               src={src[0]}
@@ -31,14 +51,14 @@ const WrapCard = styled.div`
   transition-duration: 0.5s;
   cursor: pointer;
 
-  &:hover .hovered {
+  &.active .hovered {
     display: block;
   }
-  &:nth-child(even):hover {
+  &:nth-child(even).active {
     transform: scale(1.12) translateY(-15%) translateX(12%);
     box-shadow: #00000050 -10px 10px 30px -10px;
   }
-  &:nth-child(odd):hover {
+  &:nth-child(odd).active {
     transform: scale(1.12) translateY(-15%) translateX(-12%);
     box-shadow: #00000050 10px 10px 30px -10px;
   }
