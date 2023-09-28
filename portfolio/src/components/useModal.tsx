f75e234c75c03d051fcc863735892cac988f47cf
Daniel LEE <dmsgur7112@gmail.com>
2023-09-28 13:41:26 +0900

    fix : styled component 에러 수정 및 파일 이름 변경

--- portfolio/src/components/useModal.tsx
+++ portfolio/src/components/useModal.tsx
@@ -24,8 +24,9 @@ const useModal = () => {
   const Modal = ({ children }: { children: ReactNode }) => {
     return (
       <>
-        <ModalOverlay />
+        <ModalOverlay isOpen={isOpen} />
         <ModalWrapper
+          isOpen={isOpen}
           ref={modal}
           onClick={(event) => {
             event.target === modal.current && setIsOpen(false);
@@ -37,42 +38,43 @@ const useModal = () => {
     );
   };
 
-  const ModalWrapper = styled.div`
-    height: calc(var(--vh, 1vh) * 100);
-    width: 100vw;
-    z-index: 100;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: ${isOpen ? "flex" : "none"};
-    flex-direction: column;
-    justify-content: center;
-    cursor: pointer;
-  `;
-
-  const ModalOverlay = styled.div`
-    width: 100vw;
-    height: 150vh;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    position: fixed;
-    background-color: #00000090;
-    backdrop-filter: blur(10px);
-    display: ${isOpen ? "block" : "none"};
-  `;
-
-  const ModalInner = styled.div`
-    max-height: 70%;
-    max-width: 80%;
-    aspect-ratio: 5 / 7;
-    margin: 0 auto;
-    z-index: 200;
-    border-radius: 10px;
-    overflow: hidden;
-    cursor: default;
-  `;
-
   return { Modal, setIsOpen };
 };
+
+const ModalWrapper = styled.div<{ isOpen: boolean }>`
+  height: calc(var(--vh, 1vh) * 100);
+  width: 100vw;
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: ${(props) => (props.isOpen ? "flex" : "none")};
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const ModalOverlay = styled.div<{ isOpen: boolean }>`
+  width: 100vw;
+  height: 150vh;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  position: fixed;
+  background-color: #00000090;
+  backdrop-filter: blur(10px);
+  display: ${(props) => (props.isOpen ? "block" : "none")};
+`;
+
+const ModalInner = styled.div`
+  max-height: 70%;
+  max-width: 80%;
+  aspect-ratio: 5 / 7;
+  margin: 0 auto;
+  z-index: 200;
+  border-radius: 10px;
+  overflow: hidden;
+  cursor: default;
+`;
+
 export default useModal;
