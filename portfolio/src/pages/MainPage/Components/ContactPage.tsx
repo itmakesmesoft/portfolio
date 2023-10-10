91f05ef8f9c2b7260056f98dda46c5de588f6889
Daniel LEE <dmsgur7112@gmail.com>
2023-10-10 20:38:04 +0900

    fix : contact 패딩 설정

--- portfolio/src/pages/MainPage/Components/ContactPage.tsx
+++ portfolio/src/pages/MainPage/Components/ContactPage.tsx
@@ -5,7 +5,7 @@ export const ContactPage = () => {
       id="contact"
       className="h-[50vh] w-full flex flex-col justify-center items-center bg-[#ffffff3c] py-10"
     >
-      <div className="inline-block">
+      <div className="inline-block px-8">
         <h1 className="text-2xl sm:text-3xl">CONTACT</h1>
         <Information />
       </div>
