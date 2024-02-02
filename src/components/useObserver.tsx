42a0b87617a648a892db22fda6bec87f09002b0e
Daniel LEE <dmsgur7112@gmail.com>
2024-02-02 22:23:30 +0900

    refactor : 코드 가독성 개선

--- src/components/useObserver.tsx
+++ src/components/useObserver.tsx
@@ -1,16 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const useObserver = (
-  callback: (entries: IntersectionObserverEntry[]) => unknown,
-  threshold: number
-) => {
-  const targetRef = useRef<HTMLDivElement>(null);
+const useObserver = (threshold: number) => {
+  const target = useRef<HTMLDivElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState<boolean>(false);
   useEffect(() => {
-    if (targetRef.current) {
-      const observer = new IntersectionObserver(callback, {
-        threshold: threshold,
-      });
-      observer.observe(targetRef.current as Element);
+    if (target.current) {
+      const observer = new IntersectionObserver(
+        (entries: IntersectionObserverEntry[]) => {
+          entries.forEach((entry: IntersectionObserverEntry) => {
+            if (entry.isIntersecting) setIsIntersecting(true);
+            else setIsIntersecting(false);
+          });
+        },
+        {
+          threshold: threshold,
+        }
+      );
+      observer.observe(target.current as Element);
       return () => {
         if (observer) {
           console.log("observer 종료");
@@ -20,7 +26,7 @@ const useObserver = (
     }
   }, []);
 
-  return [targetRef];
+  return { target, isIntersecting };
 };
 
 export default useObserver;
