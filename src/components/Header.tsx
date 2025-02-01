9a9ec2e68b6737212f2fb8c242c21cd163ca691c
Daniel LEE <dmsgur7112@gmail.com>
2025-02-02 00:46:24 +0900

    chore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { HashLink } from "react-router-hash-link";
+import styled from "styled-components";
 export const Header = () => {
   const menuList = [
     { to: "/#", title: "INTRO" },
@@ -9,9 +11,27 @@ export const Header = () => {
   ];
   const scrollTo = (element: Element) =>
     element.scrollIntoView({ behavior: "smooth", block: "start" });
+  const [banner, setBanner] = useState<boolean>(true);
 
   return (
     <header className="fixed top-0 left-0 flex flex-col justify-center items-center w-full z-50 text-sm sm:text-base backdrop-blur-lg bg-light-primary">
+      {banner && (
+        <div className="relative p-2 bg-[#525252] flex flex-row w-screen justify-center items-center text-white gap-8">
+          <p>이 페이지는 더 이상 관리되지 않아요.</p>
+          <Button
+            className="bg-[#52918d] px-2 py-1 hover:bg-[#62a9a4] active:bg-[#4b8581] cursor-pointer"
+            href="https://itmakesmesoft.vercel.app"
+          >
+            새로운 블로그 놀러가기
+          </Button>
+          <Button
+            className="p-1 cursor-pointer absolute right-6 top-2 text-[#cacaca] hover:text-white"
+            onClick={() => setBanner(false)}
+          >
+            닫기
+          </Button>
+        </div>
+      )}
       <div className="flex flex-row justify-between items-center w-full z-40 h-[45px] max-w-[1300px] px-4">
         <HashLink to="/#" scroll={scrollTo} className="flex-none">
           <img
@@ -42,3 +62,12 @@ export const Header = () => {
     </header>
   );
 };
+
+const Button = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.6rem;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+`;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -3,10 +3,42 @@ import { AboutSection } from "./Components/AboutSection";
 import { ContactSection } from "./Components/ContactSection";
 import Timeline from "./Components/Timeline";
 import ProjectsSection from "./Components/ProjectsSection";
+import useModal from "components/useModal";
+import { useEffect } from "react";
+import styled from "styled-components";
 
 const MainPage = () => {
+  const { Modal, isOpen, setIsOpen } = useModal();
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
   return (
     <div className="flex flex-col">
+      <Modal>
+        <div className="p-6 md:p-8 bg-white rounded-xl flex flex-col justify-center items-center">
+          <img src="/images/under-construction.png" alt="" />
+          <p className="text-lg md:text-2xl font-black my-6">
+            이 페이지는 이제 관리되지 않아요.
+          </p>
+          <p className="text-base md:text-xl mb-8">
+            블로그를 새롭게 만들었는데, 구경하러 오실래요?
+          </p>
+          <Button
+            href="https://itmakesmesoft.vercel.app"
+            rel="noopener noreferrer"
+            className="bg-[#353535] hover:bg-[#555555] text-white mb-2"
+          >
+            구경하러 갈게요!
+          </Button>
+          <Button
+            onClick={() => setIsOpen(false)}
+            rel="noopener noreferrer"
+            className="bg-[#e2e2e2] hover:bg-[#eaeaea] text-[#5f5f5f]"
+          >
+            그냥 머무를래요.
+          </Button>
+        </div>
+      </Modal>
       <IntroSection />
       <AboutSection />
       <Timeline />
@@ -17,3 +49,14 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
+const Button = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.6rem;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+  height: 2.75rem;
+  width: 100%;
+`;
